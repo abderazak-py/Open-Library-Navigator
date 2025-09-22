83a73e0860bfcd7babedd7c79f4d9c60beb54914
abderazak <achourabderazak1@gmail.com>
2025-09-22 17:24:54 +0100

    added routes

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/features/home/presentation/views/book_details_view.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
+import 'package:bookly/features/splach/presentation/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
+
+  static final GoRouter router = GoRouter(
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => const SplashView()),
+      GoRoute(path: '/homeView', builder: (context, state) => const HomeView()),
+      GoRoute(
+        path: '/bookDetailsView',
+        builder: (context, state) => const BookDetailsView(),
+      ),
+    ],
+  );
+}
--- lib/features/splach/presentation/views/widgets/splach_view_body.dart
+++ lib/features/splach/presentation/views/widgets/splach_view_body.dart
@@ -1,9 +1,8 @@
-import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets.dart';
-import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splach/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
@@ -67,11 +66,9 @@ class _SplachViewBodyState extends State<SplachViewBody>
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 3), () {
-      Get.to(
-        () => const HomeView(),
-        transition: Transition.fade,
-        duration: kTransitionDuration,
-      );
+      if (mounted) {
+        GoRouter.of(context).push(AppRouter.kHomeView);
+      }
     });
   }
 }
