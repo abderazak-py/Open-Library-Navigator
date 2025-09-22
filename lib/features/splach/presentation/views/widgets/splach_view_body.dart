9917ffb4e97c36f666aebef84a62ddb582d5ad4c
abderazak <achourabderazak1@gmail.com>
2025-09-22 09:52:26 +0100

    add animation

--- lib/features/splach/presentation/views/widgets/splach_view_body.dart
+++ lib/features/splach/presentation/views/widgets/splach_view_body.dart
@@ -1,8 +1,31 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/splach/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplachViewBody extends StatelessWidget {
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
+
+  @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initSlidingAnimation();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +34,21 @@ class SplachViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        SizedBox(height: 8),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20),
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
   }
 }
