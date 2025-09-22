461393de70fd78428808744a9e249c05cfca183c
abderazak <achourabderazak1@gmail.com>
2025-09-22 10:17:43 +0100

    improve animation, navigate home

--- lib/features/splach/presentation/views/widgets/sliding_text.dart
+++ lib/features/splach/presentation/views/widgets/sliding_text.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class SlidingText extends StatelessWidget {
-  const SlidingText({super.key, required this.slidingAnimation});
+  const SlidingText({
+    super.key,
+    required this.slidingAnimation,
+    required this.fadeAnimation,
+  });
 
   final Animation<Offset> slidingAnimation;
+  final Animation<double> fadeAnimation;
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +17,13 @@ class SlidingText extends StatelessWidget {
       builder: (context, _) {
         return SlideTransition(
           position: slidingAnimation,
-          child: const Text(
-            'Read Free Books',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20),
+          child: FadeTransition(
+            opacity: fadeAnimation,
+            child: const Text(
+              'Read Free Books',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            ),
           ),
         );
       },
--- lib/features/splach/presentation/views/widgets/splach_view_body.dart
+++ lib/features/splach/presentation/views/widgets/splach_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splach/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
@@ -13,12 +16,13 @@ class _SplachViewBodyState extends State<SplachViewBody>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation<Offset> slidingAnimation;
+  late Animation<double> fadeAnimation;
 
   @override
   void initState() {
     super.initState();
-
     initSlidingAnimation();
+    navigateToHome();
   }
 
   @override
@@ -34,7 +38,10 @@ class _SplachViewBodyState extends State<SplachViewBody>
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        SlidingText(slidingAnimation: slidingAnimation),
+        SlidingText(
+          slidingAnimation: slidingAnimation,
+          fadeAnimation: fadeAnimation,
+        ),
       ],
     );
   }
@@ -49,6 +56,22 @@ class _SplachViewBodyState extends State<SplachViewBody>
       begin: const Offset(0, 2),
       end: Offset.zero,
     ).animate(animationController);
+
+    fadeAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(animationController);
+
     animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
