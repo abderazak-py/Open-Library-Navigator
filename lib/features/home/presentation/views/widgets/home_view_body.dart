2c8d7e17d1361762c93bb169d6e3896d475d4934
abderazak <achourabderazak1@gmail.com>
2025-09-22 12:01:15 +0100

    add styles and text

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_book_list_view.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,16 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [CustomAppBar(), FeaturedBookListView()]);
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomAppBar(),
+        FeaturedBookListView(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+          child: Text('Best Seller', style: Styles.titleMedium),
+        ),
+      ],
+    );
   }
 }
