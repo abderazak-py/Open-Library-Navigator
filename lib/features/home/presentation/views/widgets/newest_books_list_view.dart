b73b7723d2cf89bb3cab05d2d5ed9df0e2ed82eb
abderazak <achourabderazak1@gmail.com>
2025-09-25 09:14:42 +0100

    moved loading_book_item

--- lib/features/home/presentation/views/widgets/newest_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/newest_books_list_view.dart
@@ -1,5 +1,5 @@
 import 'package:bookly/core/widgets/custom_error_widget.dart';
-import 'package:bookly/core/widgets/loading_book_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/loading_book_item.dart';
 import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:flutter/material.dart';
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly/core/utils/service_locator.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/core/widgets/custom_error_widget.dart';
-import 'package:bookly/core/widgets/loading_book_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/loading_book_item.dart';
 import 'package:bookly/features/search/data/repos/search_repo_impl.dart';
 import 'package:bookly/features/search/presentation/manager/cubit/search_cubit.dart';
 import 'package:bookly/features/search/presentation/views/widgets/custom_search_text_field.dart';
